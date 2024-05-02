3df5cea125aa39f19eddada3f9183f14f4d3038f
Pedro Henrique Teles <pedrohenriqueteles732@gmail.com>
2024-05-02 20:50:06 -0300

    first commit

---
+++ src/main/java/com/jobfy/JobfyApplication.java
@@ -0,0 +1,13 @@
+package com.jobfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobfyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobfy/JobfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
