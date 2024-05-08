9c54e0f8457c7ef96d23faba922a8a0de66f1bfa
Pedro Henrique Teles <pedrohenriqueteles732@gmail.com>
2024-05-08 13:19:11 -0300

    Validação Cpf e atrubuto cpf incluido, também inserido atributo nome na classe empresa

--- src/main/java/com/jobfy/models/entity/Colaborador.java
+++ src/main/java/com/jobfy/models/entity/Colaborador.java
@@ -1,10 +1,12 @@
 package com.jobfy.models.entity;
 
+import com.jobfy.validator.ValidCPF;
+import com.jobfy.validator.ValidPassword;
 import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "DB_COLABORADOR")
@@ -12,11 +14,13 @@ public class Colaborador {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotBlank(message = "O Nome do colaborador não pode estar em branco")
     @NotNull(message = "O Nome do colaborador não pode ser nulo")
     @Size(min = 3, message = "O nome do usuario deve possuir ao menos 3 caracteres")
     private String name;
 
 
+    @NotBlank(message = "O Telefone do colaborador não pode estar em branco")
     @NotNull(message = "O Telefone não pode ser nulo")
     private String telefone;
 
@@ -26,18 +30,118 @@ public class Colaborador {
     @NotNull(message = "O Salario não pode ser nulo")
     private double salario;
 
+    @NotBlank(message = "A Escolaridade do colaborador não pode estar em branco")
     @NotNull(message = "O campo escolaridade não pode ser nulo")
     private double escolaridade;
 
 
     @NotNull(message = "O Email não pode ser nulo")
     @Email(message = "O email do colaborador deve ser válido")
-    @Column
+    @Column(unique = true)
     private double email;
 
+    @NotNull(message = "A senha não pode ser nula")
+    @ValidPassword
+    private String senha;
 
+    @NotBlank(message = "A Área de Interesse  não pode estar em branco")
+    @NotNull(message = "A Área de Interesse não pode estar nula")
+    private String areaInteresse;
 
+    @NotNull(message = "A Data de Nascimento não pode ser nula")
+    @Past(message = "A Data de Nascimento deve ser no passado")
+    private Date dataNascimento;
 
+    @NotNull(message = "O CPF não pode ser nulo")
+    @Column(unique = true)
+    @ValidCPF(message = "O CPF do colaborador deve ser um CPF válido")
+    private String cpf;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public double getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(double cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getEscolaridade() {
+        return escolaridade;
+    }
+
+    public void setEscolaridade(double escolaridade) {
+        this.escolaridade = escolaridade;
+    }
+
+    public double getEmail() {
+        return email;
+    }
+
+    public void setEmail(double email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getAreaInteresse() {
+        return areaInteresse;
+    }
+
+    public void setAreaInteresse(String areaInteresse) {
+        this.areaInteresse = areaInteresse;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
--- src/main/java/com/jobfy/models/entity/Empresa.java
+++ src/main/java/com/jobfy/models/entity/Empresa.java
@@ -1,6 +1,8 @@
 package com.jobfy.models.entity;
 
 import jakarta.persistence.*;
+
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -10,5 +12,14 @@ public class Empresa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotNull(message = "O Nome não pode ser nulo")
+    @NotBlank(message = "O Nome não pode estar em branco")
+    @Size(min = 3, message = "O Nome do usuario deve possuir ao menos 3 caracteres")
+    private String name;
+
+
+
+
 
 }
